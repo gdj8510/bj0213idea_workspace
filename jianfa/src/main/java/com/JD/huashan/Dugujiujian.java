b19553f0a134430d10f3524073f404c2a27c6b1e
linghuchong <linghuchong126.com>
2020-07-11 09:09:25 +0800

    dugujiujian

---
+++ jianfa/src/main/java/com/JD/huashan/Dugujiujian.java
@@ -0,0 +1,20 @@
+package com.JD.huashan;
+
+/**
+ * @author gdj
+ * @BelongsProject: bj0213idea_workspace
+ * @BelongsPackage: com.JD.huashan
+ * @CreateTime: 2020-07-11 08:51
+ * @Description:
+ */
+public class Dugujiujian {
+    public static void main(String[] args) {
+        System.out.println("令狐冲的独孤九剑天下第一");
+        System.out.println("令狐冲的独孤九剑天下第一");
+        System.out.println("令狐冲的独孤九剑天下第一");
+        System.out.println("令狐冲的独孤九剑天下第一");
+        //TODO 红色未追踪代码 绿色本地暂存区
+
+
+    }
+}
